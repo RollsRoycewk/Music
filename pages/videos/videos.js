a7942e5f129fe7bf85e75d0b916768973b4f260e
RollsRoycewk <50765560@qq.com>
2020-12-26 13:35:31 +0800

    nav样式

--- pages/videos/videos.js
+++ pages/videos/videos.js
@@ -1,66 +1,70 @@
 // pages/videos/videos.js
+import ajax from "../../utils/ajax.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    navList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: async function() {
+    const navList = await ajax("/video/group/list")
+    this.setData({
+      navList: navList.data.splice(0, 14)
+    })
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
